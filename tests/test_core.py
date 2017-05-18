0f2bdcb9279dae09118128fa34e667b0df094941
Gailbear <gterman@gmail.com>
2017-05-18 17:50:06 -0400

    hopefully allow pygame to run headless

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,9 @@
 from dots_editor import core
+import os
 
 def test_startup():
     core.Game('test.txt', 'ascii')
     assert True
+
+def test_setenv():
+    assert os.environ["SDL_VIDEODRIVER"] == "dummy"
