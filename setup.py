da0f6e1ff92e50bd1ba8fd5a0c36f993e17e1988
Gailbear <gterman@gmail.com>
2017-05-18 19:52:09 -0400

    update pypi long description and change the name of the license file

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
+with open(path.join(here, 'README.rst'), encoding-'utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name='dots_editor',
     use_scm_version=True,
     description='A python-based six-key braille emulator',
+    long_description=long_description,
     author='Gail Terman',
     author_email='gterman@gmail.com',
     license='MIT',
